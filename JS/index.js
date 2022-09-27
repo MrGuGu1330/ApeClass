fd4ec14d6d748792ef7cd0b39a6a2d2f5793d40c
Gu <sh289218350@163.com>
2022-09-27 11:21:43 +0800

    Ape

--- JS/index.js
+++ JS/index.js
@@ -1,27 +1,24 @@
 var span = document.getElementById('fade-span');
 var flag = 0;
 var arr = $(".yc");
-var icon = $("#jt");
-var ang = 180;
-var timer;
+var icon = $(".bi-chevron-double-down");
+
 
 
 function show(){
 	for(i=0;i<arr.length;i++){
 		arr[i].style.display='block'
-	};
+	}
 	arr.animate({top:'+=170px',opacity:'1'},800);
 	span.innerHTML='关闭更多';
-	icon.removeClass('bi-chevron-double-down');
-	icon.addClass('bi-chevron-double-up');
-};
+	icon.css("transform","rotate(180deg)")
+}
 
 function hide(){
 	arr.animate({top:'-=170px',opacity:'0'},500).fadeOut(500);
 	span.innerHTML='更多课程';
-	icon.removeClass('bi-chevron-double-up');
-	icon.addClass('bi-chevron-double-down');
-};
+	icon.css("transform","rotate(0deg)")
+}
 
 
 function more(){
