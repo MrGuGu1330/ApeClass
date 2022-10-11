eed01477eef9ded383e3ed5724d379d6a1776142
Gu <sh289218350@163.com>
2022-10-11 20:25:59 +0800

    Ape

--- JS/base.js
+++ JS/base.js
@@ -1,3 +1,11 @@
+// 导航下拉菜单 课程中心
+$('.sub-menu').eq(0).click(function () {
+    console.log($(this))
+    $(this).css('display','')
+})
+// 导航下拉菜单 关于
+
+// 侧边栏
 $('.elevator .list .itemV.item-wx').mouseover(function () {
     console.log($(this))
     $('.elevator .list .itemV.item-wx>img').addClass('active')
