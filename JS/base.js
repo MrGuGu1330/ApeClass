a7edefaac8ba551fd534bff3bdff660c910701fb
Gu <sh289218350@163.com>
2022-10-21 20:10:57 +0800

    10.21

--- JS/base.js
+++ JS/base.js
@@ -1,4 +1,3 @@
-
 // 侧边栏
 $('.elevator .list .itemV.item-wx').mouseover(function () {
     $('.elevator .list .itemV.item-wx>img').addClass('active')
@@ -19,3 +18,18 @@ $('.elevator .list .itemV.item-wx').next().mouseleave(function () {
 $('.elevator .list .itemV.item-wx').next().click(function () {
     $('body').animate({scrollTop:'0'},500)
 })
+//下拉菜单1
+$('.list-left>div').eq(0).mouseover(function () {
+    $('.box-course').stop().animate({top:'60px',opacity:'1'},500)
+})
+$('.list-left>div').mouseleave(function () {
+    $('.box-course').stop().animate({top:'-400px',opacity:'0'},500)
+})
+$('.box-course').mouseover(function () {
+    $('.box-course').stop().animate({top:'60px',opacity:'1'},500)
+})
+$('.box-course').mouseleave(function () {
+    $('.box-course').stop().animate({top:'-400px',opacity:'0'},500)
+})
+
+//下拉菜单2
