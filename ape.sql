b56fecd5bf979d4128644d8e306a460ac9f11f97
Gu <sh289218350@163.com>
2022-10-31 16:20:41 +0800

    10.31

--- ape.sql
+++ ape.sql
@@ -106,4 +106,113 @@ insert into py_course values(null,'Python版本介绍','','pyc01');
 insert into py_course values(null,'编译型语言和解释型语言的区别','','pyc01');
 insert into py_course values(null,'Python的解释器','','pyc01');
 
---创建
+--创建sub表
+create table sub_left(
+    sub_id int,
+    title varchar(64),
+    type varchar(32)
+);
+
+insert into sub_left values(1,'1-从0到1学Python','Python');
+insert into sub_left values(1,'2-Python环境的安装','Python');
+insert into sub_left values(1,'3-Pycharm的安装','Python');
+insert into sub_left values(1,'4-Typora的安装','Python');
+insert into sub_left values(1,'5-Notepad++的安装','Python');
+insert into sub_left values(2,'1-前端开发是什么以及我们要学什么','前端');
+insert into sub_left values(2,'2-什么是HTML','前端');
+insert into sub_left values(2,'3-什么是CSS','前端');
+insert into sub_left values(2,'4-什么是JavaScript','前端');
+insert into sub_left values(2,'5-HBuilder使用介绍','前端');
+insert into sub_left values(3,'1-什么是数据库','数据分析');
+insert into sub_left values(3,'2-数据库的管理','数据分析');
+insert into sub_left values(3,'3-数据库的分类','数据分析');
+insert into sub_left values(3,'4-数据表的管理','数据分析');
+insert into sub_left values(3,'5-MySQL与Navicat的安装','数据分析');
+insert into sub_left values(4,'1-测试导学','软件测试');
+insert into sub_left values(4,'2-测试概述','软件测试');
+insert into sub_left values(4,'3-工作流程与项目流程','软件测试');
+insert into sub_left values(4,'4-项目各阶段产物','软件测试');
+insert into sub_left values(4,'5-项目测试计划','软件测试');
+insert into sub_left values(5,'1-学前必看-云计算课程说明','云计算');
+insert into sub_left values(5,'2-Linux开班概述','云计算');
+insert into sub_left values(5,'3-计算机基础概述','云计算');
+insert into sub_left values(5,'4-office办公软件使用','云计算');
+insert into sub_left values(5,'5-Notepad++文件编辑工具使用','云计算');
+insert into sub_left values(6,'1-办公自动化之PPT课程介绍','小课');
+insert into sub_left values(6,'2-python-pptx库简介','小课');
+insert into sub_left values(6,'3-写入文本到PPT-1','小课');
+insert into sub_left values(6,'4-写入文本到PPT-2','小课');
+insert into sub_left values(6,'5-写入图片到PPT-1','小课');
+insert into sub_left values(7,'1-第一个Java程序HelloWorld','Java(新)');
+insert into sub_left values(7,'2-第一个Java程序HelloWorld下','Java(新)');
+insert into sub_left values(7,'3-注释基本概念','Java(新)');
+insert into sub_left values(7,'4-Java关键字和保留字','Java(新)');
+insert into sub_left values(7,'5-变量和常量','Java(新)');
+insert into sub_left values(8,'1-Linux操作系统配置ip地址','云计算(新)');
+insert into sub_left values(8,'2-NFS服务搭建配置','云计算(新)');
+insert into sub_left values(8,'3-Linux基本基础命令学习','云计算(新)');
+insert into sub_left values(8,'4-vsftpd服务的安装配置','云计算(新)');
+insert into sub_left values(8,'5-Linux vim编辑器的指令使用','云计算(新)');
+insert into sub_left values(9,'1-二进制的基本概念','Java');
+insert into sub_left values(9,'2-原码补码反码概念','Java');
+insert into sub_left values(9,'3-字符集和字符编码概念','Java');
+insert into sub_left values(9,'4-Java简介','Java');
+insert into sub_left values(9,'5-JDK和jre的认识','Java');
+insert into sub_left values(10,'1-网络通信基本概念','网络工程');
+insert into sub_left values(10,'2-信息传递过程(上)','网络工程');
+insert into sub_left values(10,'3-信息传递过程(下)','网络工程');
+insert into sub_left values(10,'4-常见术语','网络工程');
+
+create table sub_right(
+    sub_id int,
+    title varchar(32),
+    type varchar(16)
+);
+
+insert into sub_right values(1,'Python环境安装','Python');
+insert into sub_right values(1,'Python基础知识','Python');
+insert into sub_right values(1,'Python进阶知识','Python');
+insert into sub_right values(1,'TCP-IP协议相关与抓包','Python');
+insert into sub_right values(1,'数据库相关','Python');
+insert into sub_right values(2,'前端基础知识','前端');
+insert into sub_right values(2,'兼容性问题与综合案例','前端');
+insert into sub_right values(2,'CSS进阶','前端');
+insert into sub_right values(2,'Bootstrap框架与移动端开发','前端');
+insert into sub_right values(2,'JavaScript相关','前端');
+insert into sub_right values(3,'数据库基础','数据分析');
+insert into sub_right values(3,'MySQL之基础篇','数据分析');
+insert into sub_right values(3,'MySQL之进阶篇','数据分析');
+insert into sub_right values(3,'Python之基础篇','数据分析');
+insert into sub_right values(3,'Python之进阶篇','数据分析');
+insert into sub_right values(4,'软件测试基础','软件测试');
+insert into sub_right values(4,'Linux操作系统','软件测试');
+insert into sub_right values(4,'数据库基础','软件测试');
+insert into sub_right values(4,'数据库进阶与实战','软件测试');
+insert into sub_right values(4,'Python编程基础','软件测试');
+insert into sub_right values(5,'Linux运维篇','云计算');
+insert into sub_right values(5,'Apache Web部署篇','云计算');
+insert into sub_right values(5,'MySQL与Redis安装部署篇','云计算');
+insert into sub_right values(5,'MySQL进阶篇','云计算');
+insert into sub_right values(5,'Zabbix+Nginx篇','云计算');
+insert into sub_right values(6,'Python办公自动化','小课');
+insert into sub_right values(6,'Python数据处理与实战','小课');
+insert into sub_right values(6,'Python微信公众号开发实战','小课');
+insert into sub_right values(6,'YOLOv4目标检测实战','小课');
+insert into sub_right values(6,'爬虫项目实战','小课');
+insert into sub_right values(7,'Java入门','Java(新)');
+insert into sub_right values(7,'Java基础','Java(新)');
+insert into sub_right values(7,'Java进阶','Java(新)');
+insert into sub_right values(7,'Java8的特性与网络编程','Java(新)');
+insert into sub_right values(7,'Java设计模式','Java(新)');
+insert into sub_right values(8,'云计算基础','云计算(新)');
+insert into sub_right values(8,'云计算进阶','云计算(新)');
+insert into sub_right values(8,'云计算高级','云计算(新)');
+insert into sub_right values(9,'Java基础','Java');
+insert into sub_right values(9,'Java进阶','Java');
+insert into sub_right values(9,'JDBC','Java');
+insert into sub_right values(9,'Java实战基础','Java');
+insert into sub_right values(9,'Spring & SpringBoot框架','Java');
+insert into sub_right values(10,'网络基础原理','网络工程');
+insert into sub_right values(10,'路由基础','网络工程');
+insert into sub_right values(10,'网络工程进阶','网络工程');
+insert into sub_right values(10,'园区项目实战','网络工程');
